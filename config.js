57c27fa111bd6aa376e87387d9dd4fd647cf115d
Diamondturtle99 <125829718+Diamondturtle99@users.noreply.github.com>
2023-06-12 15:58:17 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  apiKey: "AIzaSyB1M9ls0nG53WkAPC0li6QJ4Q285eAgD2s",
-  authDomain: "snake-a33ae.firebaseapp.com",
-  projectId: "snake-a33ae",
-  storageBucket: "snake-a33ae.appspot.com",
-  messagingSenderId: "547410064030",
-  appId: "1:547410064030:web:f09be13f5df5d02cdd418f",
-  measurementId: "G-6YZFDS5HQ0"
-};
